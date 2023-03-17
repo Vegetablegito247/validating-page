9d4e994b69b382520acca81908869e0bd583fdba
Vegetablegito247 <Yoshimitsu247Kakarot@gmail.com>
2023-03-17 22:59:27 +0100

    javashorted

--- index.js
+++ index.js
@@ -39,83 +39,24 @@ function condition(para) {
         para.style.border = '1px solid red'
         return false
     }
-    if (para.value.length < 3) {
+    else if (para.value.length < 3) {
         console.log('characters cannot be less than 3');
         para.style.border = '1px solid red';
         return false
     }
-    if (para.value.length > 30) {
+    else if (para.value.length > 30) {
         console.log('characters cannot be greater than 30');
         para.style.border = '1px solid red';
         return false
     }
-    if (para.value.length >= 3 && value.value.length <= 30){
-        para.style.border = '1px solid green';
+
+    else {
+        return true
     }
 }
 
 btn.addEventListener('click', () => {
-    if (fName.value === '') {
-        console.log('fill in your details');
-        fName.style.border = '1px solid red';
-        return false
-    }
-    else if (fName.value.length < 3) {
-        console.log('characters cannot be less than 3');
-        fName.style.border = '1px solid red';
-        return false
-    }
-    else if (fName.value.length > 30) {
-        console.log('characters cannot be greater than 30');
-        fName.style.border = '1px solid red';
-        return false
-    }
-    if (lName.value === '') {
-        console.log('fill in your details');
-        fName.style.border = '1px solid red';
-        return false
-    }
-    else if (lName.value.length < 3) {
-        console.log('characters cannot be less than 3');
-        lName.style.border = '1px solid red';
-        return false
-    }
-    else if (lName.value.length > 30) {
-        console.log('characters cannot be greater than 30');
-        lName.style.border = '1px solid red';
-        return false
-    }
-    if (email.value === '') {
-        console.log('fill in your details');
-        email.style.border = '1px solid red';
-        return false
-    }
-    else if (email.value.length < 3) {
-        console.log('characters cannot be less than 3');
-        email.style.border = '1px solid red';
-        return false
-    }
-    else if (email.value.length > 30) {
-        console.log('characters cannot be greater than 30');
-        email.style.border = '1px solid red';
-        return false
-    }
-    if (pWord.value === '') {
-        console.log('fill in your details');
-        pWord.style.border = '1px solid red';
-        return false
-    }
-    else if (pWord.value.length < 3) {
-        console.log('characters cannot be less than 3');
-        pWord.style.border = '1px solid red';
-        return false
-    }
-    else if (pWord.value.length > 30) {
-        console.log('characters cannot be greater than 30');
-        pWord.style.border = '1px solid red';
-        return false
-    }
-    else {
+    if (condition(fName) && condition(lName) && condition(email) && condition(pWord)) {
         let objValue = {
             uName: fName.value,
             ulName: lName.value,
